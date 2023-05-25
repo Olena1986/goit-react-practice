de5cdbd13414f9d6f13a526810d0133edbcb67ce
dmkomur <125804681+dmkomur@users.noreply.github.com>
2023-05-25 15:08:44 +0300

    update to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,72 +1,134 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { MoviesGallery } from './moviesGallery/moviesGallerry';
 import Button from './Button';
 import { fetchMovies } from '../services/api.js';
 import Loader from './Loader';
-import Modal from './Modal';
+import { Modal } from './Modal';
 
-export class App extends Component {
-  state = {
-    movies: [],
-    isListShow: false,
-    page: 1,
-    isLoading: false,
-    currentPoster: null,
+export const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [isListShow, setIsListShow] = useState(false);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentPoster, setCurrentPoster] = useState(null);
+
+  const changeVisibility = () => {
+    setIsListShow(prevState => !prevState);
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.isListShow !== this.state.isListShow || prevState.page !== this.state.page) {
-      this.setState({ isLoading: true });
-      fetchMovies(this.state.page)
+  const deleteMovie = id => {
+    setMovies(prevState => prevState.filter(movie => movie.id !== id));
+  };
+
+  const handleLoadMore = () => {
+    setPage(prevState => prevState + 1);
+  };
+  const handleOpenModal = data => {
+    setCurrentPoster(data);
+  };
+  const handleCloseModal = () => {
+    setCurrentPoster(null);
+  };
+
+  useEffect(() => {
+    if (isListShow) {
+      setIsLoading(true);
+      fetchMovies(page)
         .then(({ data: { results } }) =>
-          this.setState(prevState => {
-            return { movies: [...prevState.movies, ...results] };
-          })
+          setMovies(prevState => [...prevState, ...results])
         )
         .catch(error => {
           console.log(error);
         })
-        .finally(() => this.setState({ isLoading: false }));
+        .finally(() => setIsLoading(false));
     }
-  }
+  }, [isListShow, page]);
 
-  changeVisibility = () => {
-    this.setState({ isListShow: true });
-  };
+  return (
+    <>
+      {isListShow ? (
+        <>
+          <MoviesGallery
+            movies={movies}
+            onDelete={deleteMovie}
+            onClose={handleOpenModal}
+          />
+          <Button text="Load more" clickHander={handleLoadMore} />
+        </>
+      ) : (
+        <Button text="Show movies" clickHander={changeVisibility} />
+      )}
+      {isLoading && <Loader />}
+      {currentPoster && (
+        <Modal currentPoster={currentPoster} onClose={handleCloseModal} />
+      )}
+    </>
+  );
+};
 
-  deleteMovie = id => {
-    this.setState(prevState => {
-      return { movies: prevState.movies.filter(movie => movie.id !== id) };
-    });
-  };
+// export class App extends Component {
+//   state = {
+//     movies: [],
+//     isListShow: false,
+//     page: 1,
+//     isLoading: false,
+//     currentPoster: null,
+//   };
 
-  handleLoadMore=() => {
-    this.setState((prevState)=>({page: prevState.page+1}))
-  }
-  handleOpenModal=(data)=>{
-    this.setState({currentPoster: data});
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevState.isListShow !== this.state.isListShow || prevState.page !== this.state.page) {
+//       this.setState({ isLoading: true });
+//       fetchMovies(this.state.page)
+//         .then(({ data: { results } }) =>
+//           this.setState(prevState => {
+//             return { movies: [...prevState.movies, ...results] };
+//           })
+//         )
+//         .catch(error => {
+//           console.log(error);
+//         })
+//         .finally(() => this.setState({ isLoading: false }));
+//     }
+//   }
 
-  }
-  handleCloseModal=()=>{
-    this.setState({currentPoster: null});
+//   changeVisibility = () => {
+//     this.setState({ isListShow: true });
+//   };
 
-  }
+//   deleteMovie = id => {
+//     this.setState(prevState => {
+//       return { movies: prevState.movies.filter(movie => movie.id !== id) };
+//     });
+//   };
 
-  render() {
-    const { movies, isListShow, isLoading,currentPoster } = this.state;
-    return (
-      <>
-        {isListShow ? (
-          <>
-          <MoviesGallery movies={movies} onDelete={this.deleteMovie} onClose={this.handleOpenModal} />
-          <Button text='Load more' clickHander={this.handleLoadMore}/>
-          </>
-        ) : (
-          <Button text='Show movies' clickHander={this.changeVisibility} />
-        )}
-        {isLoading && <Loader />}
-        {currentPoster && <Modal currentPoster={currentPoster} onClose={this.handleCloseModal}/>}
-      </>
-    );
-  }
-}
+//   handleLoadMore=() => {
+//     this.setState((prevState)=>({page: prevState.page+1}))
+//   }
+//   handleOpenModal=(data)=>{
+//     this.setState({currentPoster: data});
+
+//   }
+//   handleCloseModal=()=>{
+//     this.setState({currentPoster: null});
+
+//   }
+
+//   render() {
+//     const { movies, isListShow, isLoading,currentPoster } = this.state;
+//     return (
+//       <>
+//         {isListShow ? (
+//           <>
+//           <MoviesGallery movies={movies} onDelete={this.deleteMovie} onClose={this.handleOpenModal} />
+//           <Button text='Load more' clickHander={this.handleLoadMore}/>
+//           </>
+//         ) : (
+//           <Button text='Show movies' clickHander={this.changeVisibility} />
+//         )}
+//         {isLoading && <Loader />}
+//         {currentPoster && <Modal currentPoster={currentPoster} onClose={this.handleCloseModal}/>}
+//       </>
+//     );
+//   }
+// }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,33 +1,62 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useEffect } from 'react';
 
-export default class Modal extends Component {
-  handleCloseEsc = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleCloseEsc);
-  }
+export const Modal = ({ onClose, currentPoster }) => {
+  useEffect(() => {
+    const handleCloseEsc = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleCloseEsc);
+    return () => {
+      window.removeEventListener('keydown', handleCloseEsc);
+    };
+  }, [onClose]);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleCloseEsc);
-  }
-
-  render() {
-    const { onClose, currentPoster } = this.props;
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <button type="button" onClick={onClose}>
-            Close
-          </button>
-          <img
-            src={`https://image.tmdb.org/t/p/w500${currentPoster.src}`}
-            alt={currentPoster.alt}
-          />
-        </div>
+        <button type="button" onClick={onClose}>
+          Close
+        </button>
+        <img
+          src={`https://image.tmdb.org/t/p/w500${currentPoster.src}`}
+          alt={currentPoster.alt}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+// export default class Modal extends Component {
+//   handleCloseEsc = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleCloseEsc);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleCloseEsc);
+//   }
+
+//   render() {
+//     const { onClose, currentPoster } = this.props;
+//     return (
+//       <div>
+//         <div>
+//           <button type="button" onClick={onClose}>
+//             Close
+//           </button>
+//           <img
+//             src={`https://image.tmdb.org/t/p/w500${currentPoster.src}`}
+//             alt={currentPoster.alt}
+//           />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
